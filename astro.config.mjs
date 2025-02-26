676c67dea4d388870e7b590d82c71f2b63917008
Anton <Sinja9494@gmail.com>
2025-02-26 20:20:18 +0100

    llcom

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'antonorlov.com',
+	site: 'https://antonorlov.com',
 	integrations: [mdx(), sitemap()],
 });
