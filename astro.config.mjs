06cdff675c59eff1646b0d25b83a1ae789d1c7a0
Anton <Sinja9494@gmail.com>
2025-03-02 18:55:53 +0100

    some change

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,17 +2,13 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
-
 import react from '@astrojs/react';
 import { getCollection } from 'astro:content';
-
 import tailwindcss from '@tailwindcss/vite';
 
-// https://astro.build/config
 export default defineConfig({
   site: 'https://antonorlov.com',
   integrations: [mdx(), sitemap(), react()],
-
   vite: {
     plugins: [tailwindcss()],
     resolve: {
