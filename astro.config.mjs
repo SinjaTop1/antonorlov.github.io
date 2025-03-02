782586451fff6fcfbead512e26fce1d6e6b38cb2
Anton <Sinja9494@gmail.com>
2025-03-02 18:59:37 +0100

    deleted something

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,6 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import react from '@astrojs/react';
-import { getCollection } from 'astro:content';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
