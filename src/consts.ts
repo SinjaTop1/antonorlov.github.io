b55ae6ceb8aac29204e69c496204daee7ac4fc7f
Anton <Sinja9494@gmail.com>
2025-03-04 13:13:30 +0100

    added meta tags

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'Anton Orlov';
-export const SITE_DESCRIPTION = 'Welcome to my personal website!';
+export const SITE_DESCRIPTION = 'Personal website of Anton Orlov, showcasing projects and insights on entrepreneurship and innovation.';
